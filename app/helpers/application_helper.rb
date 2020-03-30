beb9e8fa807b643f45213bbd061f1b70495e9502
Dominik M. Kwiatek <dom@dom.dog>
2020-03-30 23:21:12 +0100

    Fix delisp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,9 @@ module ApplicationHelper
   # All methods defined here can be used in views.
 
   def wiki(name, delisp: true)
-    name.sub!(/\s+\(.+\)/, "") if delisp
-
     haml_tag(:a, href: "https://en.wikipedia.org/wiki/#{name.gsub(' ', '_')}") do
+      name.sub!(/\s+\(.+\)/, "") if delisp
+
       haml_concat name
     end
   end
