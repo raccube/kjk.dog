d572c879ba2c9fa6827db4028f772d550e6e1008
Dominik M. Kwiatek <dom@dom.dog>
2020-03-30 23:03:15 +0100

    Add delisp option to wiki helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,9 @@
 module ApplicationHelper
   # All methods defined here can be used in views.
 
-  def wiki(name)
+  def wiki(name, delisp: true)
+    name.sub!(/\s+\(.+\)/, "") if delisp
+
     haml_tag(:a, href: "https://en.wikipedia.org/wiki/#{name.gsub(' ', '_')}") do
       haml_concat name
     end
