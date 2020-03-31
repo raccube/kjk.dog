54d5f43ce66637202c20928774fe42721181deaa
Dominik M. Kwiatek <dom@dom.dog>
2020-03-31 19:22:34 +0100

    Link to mobile Wikipedia instead
    It's a lot cleaner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   # All methods defined here can be used in views.
 
   def wiki(name, delisp: true)
-    haml_tag(:a, href: "https://en.wikipedia.org/wiki/#{name.gsub(' ', '_')}") do
+    haml_tag(:a, href: "https://en.m.wikipedia.org/wiki/#{name.gsub(' ', '_')}") do
       name.sub!(/\s+\(.+\)/, "") if delisp
 
       haml_concat name
